39b49e8a47c7d857f87546db9b7004c64c28189b
mlgmontage <kettleofrassel1@gmail.com>
2020-11-17 12:23:13 +0500

    header & bugfix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function App() {
-  return <div>Hello, Markdown | Blog</div>;
+  return (
+    <div className="container">
+      <header className="text-center my-4">
+        <h1>
+          <i className="fa fa-asterisk"></i>
+        </h1>
+        <h1>Markdown Blogpost</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
