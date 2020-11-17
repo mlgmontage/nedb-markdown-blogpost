216622821239bb87af6a16017f255cc24d046445
mlgmontage <kettleofrassel1@gmail.com>
2020-11-17 11:45:59 +0500

    removing axios & bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,7 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import Posts from "./components/posts";
 
 function App() {
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>Hello world</h1>
-          <Posts />
-        </Col>
-      </Row>
-    </Container>
-  );
+  return <div>Hello, Markdown | Blog</div>;
 }
 
 export default App;
